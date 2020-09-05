82d199a36583ece3e7d8330e14fd16d42ad77d7a
Phy Lieng <phylieng97@gmail.com>
2020-09-05 17:10:49 +0700

    new_code<algorithm> rotation of array

---
+++ array_rotation.js
@@ -0,0 +1,11 @@
+function arrayRotation(A, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    rotation_value = K % A.length;
+    left_array = A.splice(A.length - rotation_value);
+    return [...left_array,...A];
+    
+};
+A = [1,1,2,3,5];
+K = 7;
+const result = arrayRotation(A, K);
+console.log(result);
